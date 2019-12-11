e2a3e470a3581f61fbdf243ce4cb070392989b7e
Tuan Dat <tuandat99kk@gmail.com>
2019-12-11 16:08:00 +0700

    Initial commit

---
+++ app/src/main/java/com/teamnaqq/androidbaberbooking/Model/CommentItem.java
@@ -0,0 +1,46 @@
+package com.teamnaqq.androidbaberbooking.Model;
+
+public class CommentItem {
+
+    private String time,date,cmt,customer,imgCmt;
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getCmt() {
+        return cmt;
+    }
+
+    public void setCmt(String cmt) {
+        this.cmt = cmt;
+    }
+
+    public String getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(String customer) {
+        this.customer = customer;
+    }
+
+    public String getImgCmt() {
+        return imgCmt;
+    }
+
+    public void setImgCmt(String imgCmt) {
+        this.imgCmt = imgCmt;
+    }
+}
